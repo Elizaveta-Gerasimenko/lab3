4366a0beea3419ec1a4944189252ac7d4b7f5126
OOP Nust MISIS <>
2025-03-27 13:13:28 +0000

    Initial commit

---
+++ include/library.hpp
@@ -0,0 +1,12 @@
+#ifndef NOTIFY_H
+#define NOTIFY_H
+
+#include <iostream>
+#include <string>
+
+namespace template_library {
+
+// Реализуйте код классов и опеределение методов здесь
+} 
+
+#endif 
