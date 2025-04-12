fc255676b3f41ba15a6d7dd57fdec896f8fe9477
Liza <m2401744@edu.misis.ru>
2025-04-12 14:36:59 +0300

    -

--- include/library.hpp
+++ include/library.hpp
@@ -6,7 +6,30 @@
 
 namespace template_library {
 
-// Реализуйте код классов и опеределение методов здесь
+    class NotifierBase {
+    public:
+        virtual ~NotifierBase() = default;
+        virtual void Notify(const std::string& message) const = 0;
+    };
+
+    class SmsNotifier : public NotifierBase {
+    public:
+        SmsNotifier(const std::string& phone_number) : phone_number_(phone_number) {}
+        void Notify(const std::string& message) const override;
+
+    private:
+        std::string phone_number_;
+    };
+
+    class EmailNotifier : public NotifierBase {
+    public:
+        EmailNotifier(const std::string& email) : email_(email) {}
+        void Notify(const std::string& message) const override;
+
+    private:
+        std::string email_;
+    };
+
 } 
 
-#endif 
+#endif
--- src/library.cpp
+++ src/library.cpp
@@ -1,17 +1,24 @@
-#include "library.hpp"
+#include "Library.hpp"
 
 namespace template_library {
 
-// Функция для отправки смс. Не менять!
-void SendSms(const std::string& number, const std::string& message) {
-    std::cout << "Send '" << message << "' to number " << number << std::endl;
-}
+    // Функция для отправки смс. Не менять!
+    void SendSms(const std::string& number, const std::string& message) {
+        std::cout << "Send '" << message << "' to number " << number << std::endl;
+    }
 
-// Функция для отправки email. Не менять!
-void SendEmail(const std::string& email, const std::string& message) {
-    std::cout << "Send '" << message << "' to e-mail " << email << std::endl;
-}
+    // Функция для отправки email. Не менять!
+    void SendEmail(const std::string& email, const std::string& message) {
+        std::cout << "Send '" << message << "' to e-mail " << email << std::endl;
+    }
 
-// Реализация методов классов ниже...
+    // Реализация методов классов
+    void SmsNotifier::Notify(const std::string& message) const {
+        SendSms(phone_number_, message);
+    }
+
+    void EmailNotifier::Notify(const std::string& message) const {
+        SendEmail(email_, message);
+    }
 
 } 
